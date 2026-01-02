13d72cba1fb88a17e01d7dfd23b32db32d0976a9
natsuki1585 <natsukiwaseda@ruri.waseda.jp>
2026-01-02 10:29:36 +0900

    script.js ろーローカル開発対応修正

--- docs/script.js
+++ docs/script.js
@@ -15,7 +15,8 @@ const btnPrev = document.getElementById("btnPrev");
 const btnReset = document.getElementById("btnReset");
 
 // ローカル開発ならこれ
-const ws = new WebSocket("ws://${location.host}");
+const wsProto = location.protocol === "https:" ? "wss" : "ws";
+const ws = new WebSocket(`${wsProto}://${location.host}`);
 
 // Pages(https)に置くなら最終的に wss:// にする必要があります
 // 例：const ws = new WebSocket("wss://あなたのサーバードメイン");
